7560c17f662205cdd99c421c5b303059da64d6ab
Yannick Lohse <glorf.travian@gmail.com>
2013-01-06 13:36:45 +0100

    Basic bouncing mechanism

--- objects.js
+++ objects.js
@@ -1,8 +1,30 @@
-var P3l = Cannon.Circle.extend({
+var P3l = Cannon.Display.Circle.extend({
 	__construct: function(x, y){
 		this._super(false);
 		this.x = (x || 0);
 		this.y = (y || 0);
-		this.radius = 10;
+		this.radius = 5;
+		
+		this.bounces = 0;
+		
+		this.direction= new Vector2D(0, 0);
 	},
+	bounceOff: function(){
+		this.bounces++;
+		
+		this.direction.y = Cannon.Math.Utils.randomIn(-19, -10);
+		this.direction.x = GUTTER_WIDTH/this.predict(this.y, this.direction.y);
+	},
+	predict: function(y, vy){
+		var counter = 0;
+		
+		do{
+			vy += GRAVITY;
+			y += vy;
+			counter++;
+		}
+		while(y < paddle.y);
+		
+		return counter;
+	}
 });
--- p3l.js
+++ p3l.js
@@ -1,10 +1,69 @@
 Cannon.include('http://code.yannick-lohse.fr/cannon/1/display.js');
+Cannon.include('http://code.yannick-lohse.fr/cannon/1/misc.js');
+Cannon.include('http://code.yannick-lohse.fr/cannon/1/math.js');
 
-var canvas;
+var canvas, background;
+var paddle, pels = [];
+var GRAVITY = .4, 
+	GUTTER_WIDTH = 100;
 
 Cannon.onReady = function(){
 	Cannon.Logger.autolog('logs');
 	Cannon.use('*');
 	
+	Cannon.getScript('objects.js', init);
+	
 	canvas = new Cannon.Canvas('canvas');
+	
+	background = new Rectangle(0, 0, canvas.width, canvas.height);
+	background.fillStyle = new Color(209, 210, 215);
+	canvas.addChild(background);
+	
+	paddle = new Rectangle(25, canvas.height-20, 100, 10);
+	canvas.addChild(paddle);
+	paddle.fillStyle = '#4e4f53';
+	paddle.strokeStyle = '#4e4f53';
+	
+	var titleText = new DynamicText('p3l', 0, 50);
+	titleText.setFont('arial', 80, '');
+	canvas.addChild(titleText);
+	titleText.x = canvas.width/2 - titleText.getWidth()/2;
+	titleText.fillStyle = '#babbc1';
+	
+	canvas.on('canvas:render', onRender);
 };
+
+function onRender(){
+	for (var i = 0; i < pels.length; i++){
+		var pel = pels[i];
+		
+		pel.direction.y = pel.direction.y+GRAVITY;
+		//pel.direction.y = Math.min(pel.direction.y+GRAVITY, PEL_MAX_SPEED);
+		
+		pel.x += pel.direction.x;
+		pel.y += pel.direction.y;
+		
+		if (pel.y >= paddle.y){
+			pel.bounceOff();
+		}
+	}
+}
+
+function predict(y, vectorY){
+	var counter = 0;
+	
+	do{
+		vectorY += GRAVITY;
+		y += vectorY;
+		counter++;
+	}
+	while(y < paddle.y);
+	
+	return counter;
+}
+
+function init(){
+	var pel = new P3l(75, 50);
+	canvas.addChild(pel);
+	pels.push(pel);
+}
