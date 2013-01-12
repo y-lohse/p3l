dd4adb472b3bb4d89d92c984164e899652e6fbcc
Yannick Lohse <glorf.travian@gmail.com>
2013-01-12 17:21:49 +0100

    Duplicated paddle sounds and renamed

--- sounds.js
+++ sounds.js
@@ -1,6 +1,8 @@
 var P3lSounds = {
 	loaded: false,
+	duplicates: 5,
 	paddleSound: null,
+	paddleSounds: [],
 	init: function(){
 		var audiotest = new Audio();
 		var format = (audiotest.canPlayType('audio/mpeg')) ? 'mp3' : 'wav';
@@ -13,13 +15,19 @@ var P3lSounds = {
 		loadingSet.add(this.paddleSound);
 		this.paddleSound.addEventListener('canplaythrough', Cannon.Utils.bind(function(){
 			loadingSet.ready(this.paddleSound);
+			
+			for (var i = 0; i < this.duplicates; i++){
+				this.paddleSounds.push(new Audio('sounds/0.'+format));
+			}
+			this.paddleSound = 0;
 		}, this), false);
 		this.paddleSound.load();
 	},
 	playSound: function(sound){
 		if (!this.loaded) return;
 		
-		this.paddleSound.currentTime = 0;
-		this.paddleSound.play();
+		this.paddleSounds[this.paddleSound].currentTime = 0;
+		this.paddleSounds[this.paddleSound].play();
+		if (++this.paddleSound >= this.duplicates) this.paddleSound = 0;
 	}
 };
